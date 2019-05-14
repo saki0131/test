dd8bdb63700c4e61ba954ccf949ecfdef54e3acb
saki0131 <fleur.0901@gmail.com>
2019-05-14 22:25:28 +0900

    test version01

---
+++ sample/src/jp/co/internous/action/Main.java
@@ -0,0 +1,33 @@
+package jp.co.internous.action;
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println(gokei());
+		System.out.println(gokei2(2,3));
+		System.out.println(gokei2(5,7));
+		System.out.println(circle(5));
+		System.out.println(hikizan(10,10));
+		System.out.println(kakezan(10,10));
+		System.out.println(warizan(10,10));
+	}
+	public static int gokei(){
+		return 1+1;
+	}
+	public static int gokei2(int number1,int number2){
+		return number1+number2;
+	}
+	public static double circle(int hankei){
+		return hankei*hankei*3.14;
+	}
+	public static int hikizan(int number1,int number2){
+		return number1-number2;
+	}
+	public static int kakezan(int number1,int number2){
+		return number1*number2;
+	}
+	public static int warizan(int number1,int number2){
+		return number1/number2;
+	}
+}
